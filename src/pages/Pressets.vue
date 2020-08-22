015af77c99370b79b07edf38022b4c59124e01c0
Roberth González <rgonzalez.daw18@lamerce.academy>
2020-08-22 16:16:28 +0200

    Default pressets

--- src/pages/Pressets.vue
+++ src/pages/Pressets.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>Pressets</h1>
+  <div class="p-4">
+    <ul
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4"
+    >
+      <li
+        class="cursor-pointer hover:shadow-lg transition duration-100 ease-in"
+        v-for="(presset, index) in pressets"
+        :key="index"
+      >
+        <div
+          class="flex justify-center items-center h-48 rounded"
+          :class="presset"
+        ></div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { pressets } from "../pressets";
+
 export default {
   name: "Pressets",
+  data: () => {
+    return {
+      pressets: [],
+    };
+  },
+  mounted() {
+    this.pressets = pressets;
+  },
 };
 </script>
---
+++ src/pressets.js
@@ -0,0 +1,7 @@
+export const pressets = [
+  "bg-gradient-to-r from-teal-400 to-blue-500",
+  "bg-gradient-to-r from-teal-100 via-teal-200 to-green-300",
+  "bg-gradient-to-tr from-blue-600 via-purple-600 to-orange-500",
+  "bg-gradient-to-tr from-pink-400 via-blue-300 to-green-500",
+  'bg-gradient-to-r from-indigo-400 via-indigo-900 to-purple-500'
+];
