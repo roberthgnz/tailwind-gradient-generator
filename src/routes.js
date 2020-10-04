febd317092fc9976f8f544ee95a8bb39129e6a0d
keithmchd48 <keithmchd48@gmail.com>
2020-10-04 19:39:57 +0530

    Gradient can be generated with colors entered in the URL

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,13 @@ import Pressets from "./pages/Pressets";
 const routes = [
   { path: "/", component: Home },
   { path: "/pressets", component: Pressets },
+  {
+    path: "/gradient",
+    name: 'user-gradient',
+    component: Home,
+    // user will be able to enter the url as "/gradient?colors=red+teal+indigo"
+    props: route => ({ query: route.query.colors })
+  },
 ];
 
 export default routes;
