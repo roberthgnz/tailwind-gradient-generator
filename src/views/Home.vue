dcc4296eb5e7ac9f7cdfa04c6545bfa4795e7999
Eliut González <reliutg@gmail.com>
2021-08-12 22:50:02 +0200

    feat: copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -47,7 +47,27 @@
 
     <template v-if="gradient">
       <div class="relative" style="height: 33.33vh" :class="classes">
-        <div class="code-preview">{{ classes }}</div>
+        <div title="Copy code" class="code-preview cursor-pointer" @click="copyClasses">
+          <svg
+            v-if="!copied"
+            xmlns="http://www.w3.org/2000/svg"
+            width="44"
+            height="44"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <rect x="8" y="8" width="12" height="12" rx="2" />
+            <path
+              d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"
+            />
+          </svg>
+          <span v-else>🎉 copied</span>
+        </div>
         <direction :direction="direction" @click="handleDirection"></direction>
       </div>
     </template>
