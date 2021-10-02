745553c26324ea9e98062d2395c26aa37971baa7
eliutgon <reliutg@gmail.com>
2021-10-02 13:24:37 +0200

    chore: git hooks with husky

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -49,7 +49,7 @@ export const debounce = (func, wait, immediate) => {
 }
 
 // Generates a random integer between min(inclusive) & max(inclusive): [min, max]
-export const getRandomInt= (min, max) => {
+export const getRandomInt = (min, max) => {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
