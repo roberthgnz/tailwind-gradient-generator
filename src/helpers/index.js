76bc0de5d5bdecc6210408a615ac9c51b1d4bbc8
keithmchd48 <keithmchd48@gmail.com>
2021-10-02 15:30:13 +0530

    feat: random gradient generation

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -49,3 +49,10 @@ export const debounce = (func, wait, immediate) => {
     if (callNow) func.apply(context, args);
   };
 }
+
+// generates a random integer between min(inclusive) & max(inclusive): [min, max]
+export const getRandomInt = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
