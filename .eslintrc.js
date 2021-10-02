be00f4642670af4641e5b966bd24a7ed42e15ae3
eliutgon <reliutg@gmail.com>
2021-10-02 12:47:22 +0200

    chore: script for lint added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,12 @@
 module.exports = {
-  extends: [
-    'plugin:vue/recommended',
-    'plugin:prettier-vue/recommended',
-    'prettier',
-  ],
+  extends: ['plugin:vue/recommended', 'plugin:prettier-vue/recommended', 'prettier'],
   rules: {
     'prettier-vue/prettier': [
       'error',
       {
         // Override all options of `prettier` here
         // @see https://prettier.io/docs/en/options.html
+        printWidth: 120,
         singleQuote: true,
         semi: false,
         trailingComma: 'es5',
