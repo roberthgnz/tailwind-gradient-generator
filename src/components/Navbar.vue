a6f3263c0e649c84f91f49c247ce7cffbef65183
Eliut González <reliutg@gmail.com>
2021-10-27 19:38:47 +0200

    feat: improve info

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,11 +24,13 @@
           d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
         />
       </svg>
-      <router-link to="/">
-        <span class="font-semibold text-xl tracking-tight dark:text-white">
-          Tailwind Gradient Generator
-        </span>
-      </router-link>
+      <h1>
+        <router-link to="/">
+          <span class="font-semibold text-xl tracking-tight dark:text-white">
+            Tailwind Gradient Generator
+          </span>
+        </router-link>
+      </h1>
     </div>
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
       <div class="text-sm lg:flex-grow">
