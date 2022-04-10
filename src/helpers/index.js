8bbca12fbfd88359ed21c9ca867d1f1ee8dc0269
eliutdev <reliutg@gmail.com>
2022-04-10 23:06:26 +0200

    refactro: copy to clipboard

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,24 +1,8 @@
 export const copyToClipboard = (text, callback = null) => {
-  function selectElementText(element) {
-    if (document.selection) {
-      let range = document.body.createTextRange();
-      range.moveToElementText(element);
-      range.select();
-    } else if (window.getSelection) {
-      let range = document.createRange();
-      range.selectNode(element);
-      window.getSelection().removeAllRanges();
-      window.getSelection().addRange(range);
-    }
+  navigator.clipboard.writeText(text);
+  if (callback) {
+    callback();
   }
-
-  let element = document.createElement("div");
-  element.textContent = text;
-  document.body.appendChild(element);
-  selectElementText(element);
-  document.execCommand("copy");
-  element.remove();
-  if (callback !== null) callback();
 };
 
 export const addClassesToLocalStorage = (classes, database) => {
