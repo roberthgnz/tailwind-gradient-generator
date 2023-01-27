0fa8924048f412da648850689e287ae26cc7eadd
rxb3rth <reliutg@gmail.com>
2023-01-27 20:31:40 +0100

    feat: add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
