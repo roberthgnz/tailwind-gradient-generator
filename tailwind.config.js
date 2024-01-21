38b53acd4a62b6bee31dcf4860c6cf9a6f6d724b
Roberth González <reliutg@gmail.com>
2024-01-21 16:15:01 +0100

    feat: a gl font plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,59 +1,63 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./src/**/*.vue", "./public/**/*.html"],
-  safelist: [
-    {
-      pattern: /^bg-gradient-to-/,
+    content: ['./src/**/*.vue', './public/**/*.html'],
+    safelist: [
+        {
+            pattern: /^bg-gradient-to-/,
+        },
+        {
+            pattern: /^from-/,
+        },
+        {
+            pattern: /^via-/,
+        },
+        {
+            pattern: /^to-/,
+        },
+        {
+            pattern: /^bg-/,
+        },
+    ],
+    darkMode: 'class',
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: '#000',
+            white: '#fff',
+            slate: colors.slate,
+            gray: colors.gray,
+            neutral: colors.neutral,
+            stone: colors.stone,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+        },
+        extend: {
+            rotate: {
+                135: '135deg',
+                '-135': '-135deg',
+            },
+            fontFamily: {
+                sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    {
-      pattern: /^from-/,
-    },
-    {
-      pattern: /^via-/,
-    },
-    {
-      pattern: /^to-/,
-    },
-    {
-      pattern: /^bg-/,
-    },
-  ],
-  darkMode: "class",
-  theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: "#000",
-      white: "#fff",
-      slate: colors.slate,
-      gray: colors.gray,
-      neutral: colors.neutral,
-      stone: colors.stone,
-      red: colors.red,
-      orange: colors.orange,
-      amber: colors.amber,
-      yellow: colors.yellow,
-      lime: colors.lime,
-      green: colors.green,
-      emerald: colors.emerald,
-      teal: colors.teal,
-      cyan: colors.cyan,
-      sky: colors.sky,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      violet: colors.violet,
-      purple: colors.purple,
-      fuchsia: colors.fuchsia,
-      pink: colors.pink,
-      rose: colors.rose,
-    },
-    extend: {
-      rotate: {
-        135: "135deg",
-        "-135": "-135deg",
-      },
-    },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import webfontDownload from 'vite-plugin-webfont-dl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), webfontDownload()],
 })
